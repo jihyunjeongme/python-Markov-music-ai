a5fdf8c491135c3a9a26f96e33791ebcb431a9c9
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-04-20 19:17:15 +0900

    app.coding

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
-import pysynth as py
+import pysynth as ps
 from pyknon.genmidi import Midi
+from pyknon.music import NoteSeq, Note, Rest
+from src.MarkovMusic import MusicMatrix
+from pprint import pprint
+
